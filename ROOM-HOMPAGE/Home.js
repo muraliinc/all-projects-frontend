fee01538c29fb575b240a43ae4a01368c7cfa384
muraliinc <muraliintex786@gmail.com>
2025-03-27 21:16:05 +0530

    desktop front and back button done

---
+++ ROOM-HOMPAGE/Home.js
@@ -0,0 +1,47 @@
+//below is for button left and right
+
+let index_value = 0;
+
+const images = [
+  "ASSESTS/desktop-image-hero-1.jpg",
+  "ASSESTS/desktop-image-hero-2.jpg",
+  "ASSESTS/desktop-image-hero-3.jpg",
+];
+
+const heading_title = [
+  "Discover innovative ways to decorate ",
+  "We are available all across the globe",
+  "Manufactured with the best materials",
+];
+
+const paragraph_tag = [
+  "we provide unmatched quality , comfort, and style for property owners across the country. Our exports combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
+  "With stores all cover the world,it's easy for you to find the furniture for your home or place of business.Locally,we're in most major cities throughout the country, Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
+  "Our modern furniture store provide a high level of quality . Our company has invested in advanced technology to ensure that every poduct is made as perfect and as consitent as possible. With three decades of experience in this industry,we undestand what customers want for their home and office."
+];
+
+function nextimage(){
+    if(index_value < images.length -1){
+        index_value++ ;
+    }
+    console.log("clicked right");
+    
+    updatevalue();
+}
+
+function previmage(){
+    if (index_value > 0) {
+        index_value--;
+    }
+    console.log("left clicked");
+    
+    updatevalue();
+  }
+
+function updatevalue (){
+    document.querySelector(".hero-image1").src= images[index_value];
+    document.querySelector(".heading-title").textContent = heading_title[index_value];
+    document.querySelector(".p-tag").textContent = paragraph_tag[index_value];
+}
+
+// below is for hamburger menu
