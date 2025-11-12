0b70a464b472cf76e539915d318a6d1cdd74e3d7
muraliinc <muraliintex786@gmail.com>
2025-11-12 14:10:02 +0530

    hero section started

--- Portfolio-MURALI/src/App.jsx
+++ Portfolio-MURALI/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Hero from "./components/HERO/Hero";
 import Navbar from "./components/NAVBAR/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero/>
     </>
   );
 }
--- Portfolio-MURALI/src/components/HERO/Hero.jsx
+++ Portfolio-MURALI/src/components/HERO/Hero.jsx
@@ -1,5 +1,27 @@
-import insta_icon from "./assets/icons/instgram.png";
-import linkdin from "./assets/icons/Linkdin-icon.png";
-import whatsapp from "./assets/icons/whatsapp-icon.png";
-import murali2 from "./assets/murali-name2.png"; 
-import batman_hero  from "./assets/batman-hero.png";
+import insta_icon from "../../assets/icons/instgram.png";
+import linkdin from "../../assets/icons/Linkdin-icon.png";
+import whatsapp from "../../assets/icons/whatsapp-icon.png";
+import murali2 from "../../assets/murali-name2.png"; 
+import batman_hero  from "../../assets/batman-hero.png";
+import murali_image from "../../assets/HERO-NAME-IMG.png"
+import "./Hero.css";
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className="hero-section">
+        {/*HERE VIDEO NEED TO BE INCLUDED HER FOR THE HERO SECTION WIDTH SIZE  */}
+    <div className="hero-sectionWrapper">
+        <img className="murali-logo" src={murali_image} alt="hero-name" />
+
+        <div className="title-container">
+            <h2></h2>
+
+        </div>
+
+
+        </div>
+      
+    </div>
+  )
+}
