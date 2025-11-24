2e4729b109183d19940a345908f60490580b9811
muraliinc <muraliintex786@gmail.com>
2025-11-24 13:56:50 +0530

    swipper js modifying

--- Portfolio-MURALI/src/components/About/SLIDES/Slides2.jsx
+++ Portfolio-MURALI/src/components/About/SLIDES/Slides2.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import boxing_img from "../../../assets/boxing-pic-about.jpg"
 import {Swiper, SwiperSlide} from "swiper/react";
+import { EffectCoverflow, Pagination } from 'swiper/modules';
 import "swiper/css"
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
 import {sliderimages4}  from "./SlidesData"
 
 
@@ -19,6 +22,71 @@ export default function Slides2() {
                  <img className='boxing-img-about' src={boxing_img} alt="boxing-img" />
               </div>  
               <div className='slide2-about-flex'>
+                {/* <Swiper  
+                  modules={[EffectCoverflow, Pagination]}
+                   effect="coverflow"
+                spaceBetween={20}
+                slidesPerView={1} 
+                centeredSlides={true}
+                pagination={{clickable: true}}
+                coverflowEffect={{
+                  rotate:50,
+                  stretch:10,
+                  depth:100,
+                  modifier :1,
+                  slideShadows:true
+                }}
+
+
+                style={{width: "220px" ,height:"250px", borderRadius :"20px",overflow:"visible"}}>
+                  {sliderimages4.map((img,index)=>(
+                    <SwiperSlide key={index}>
+                      <img 
+                      src={img}
+                      style={{width:"100%",
+                         height:"100%",
+                         borderRadius:"20px",
+                         objectFit:"cover"
+                      }}
+                      />
+
+
+                    </SwiperSlide>
+                  ))}
+
+                </Swiper> */}
+
+                <Swiper
+  modules={[EffectCoverflow, Pagination]}
+  effect="coverflow"
+  spaceBetween={-150}
+  slidesPerView={1.6}
+  centeredSlides={true}
+  grabCursor={true}
+  pagination={{ clickable: true }}
+  coverflowEffect={{
+    rotate: 0,
+    stretch: 0,
+    depth: 120,
+    modifier: 1,
+    slideShadows: false,
+  }}
+  className="swiper-container-custom"
+>
+  {sliderimages4.map((img, index) => (
+    <SwiperSlide key={index}>
+      <img
+        src={img}
+        style={{
+          width: "100%",
+          height: "100%",
+          borderRadius: "20px",
+          objectFit: "cover",
+        }}
+      />
+    </SwiperSlide>
+  ))}
+</Swiper>
 
          
                 
